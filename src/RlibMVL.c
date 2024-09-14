f1785ff25ba5ad8aab688ad904bc2dc7b0011533
Vladimir Dergachev <support@altumrete.com>
2024-09-14 13:40:02 +0000

    version 1.1.0.1

--- src/RlibMVL.c
+++ src/RlibMVL.c
@@ -43,7 +43,7 @@ typedef struct {
 	} MMAPED_LIBRARY;
 	
 MMAPED_LIBRARY *libraries=NULL;
-int libraries_size=0;
+size_t libraries_size=0;
 int libraries_free=0;
 
 SEXP mmap_library(SEXP filename, SEXP mode0)
@@ -4180,14 +4180,14 @@ switch(TYPEOF(sexp)) {
 
 SEXP get_neighbors(SEXP spatial_index, SEXP data_list) 
 {
-LIBMVL_OFFSET64 data_offset, index_offset, *query_mark, Nv, N2, indices_size, indices_free, *indices;
+LIBMVL_OFFSET64 data_offset, index_offset, *query_mark, Nv, N2, indices_size, indices_free, *indices, ball_size, Nbits;
 int data_idx, index_idx;
 LIBMVL_VECTOR *vec_bits, *vec_first, *vec_first_mark, *vec_prev_mark, *vec_mark, *vec_prev, *vec, *vec_stats, *vec_max_count;
 SEXP ans, sa;
 double *values, *pd;
 LIBMVL_NAMED_LIST *L;
-int *bits, Nbits;
-long long *first, *first_mark, *prev_mark, *mark, *prev, max_count, ball_size;
+int *bits;
+long long *first, *first_mark, *prev_mark, *mark, *prev, max_count;
 LIBMVL_VEC_STATS *vstats;
 char *ball;
 
@@ -4240,7 +4240,7 @@ Nbits=mvl_vector_length(vec_bits);
 
 if(Nbits!=length(data_list) || length(data_list)==0) {
 	mvl_free_named_list(L);
-	error("Query columns do not match spatial index: %d vs %d", length(data_list), Nbits);
+	error("Query columns do not match spatial index: %d vs %llu", length(data_list), Nbits);
 	return(R_NilValue);
 	}
 	
@@ -4253,7 +4253,7 @@ if(Nbits*sizeof(LIBMVL_VEC_STATS)!=mvl_vector_length(vec_stats)*sizeof(double)) 
 /* This cutoff is determined by the call to calloc() to allocate ball below */
 if(Nbits>36) {
 	mvl_free_named_list(L);
-	error("Nbits=%d is too large (>36)", Nbits);
+	error("Nbits=%llu is too large (>36)", Nbits);
 	return(R_NilValue);
 	}
 	
@@ -4418,14 +4418,14 @@ return(ans);
 
 SEXP neighbors_lapply(SEXP spatial_index, SEXP data_list, SEXP fn, SEXP env) 
 {
-LIBMVL_OFFSET64 data_offset, index_offset, *query_mark, Nv, N2, indices_size, indices_free, *indices;
+LIBMVL_OFFSET64 data_offset, index_offset, *query_mark, Nv, N2, indices_size, indices_free, *indices, Nbits, ball_size;
 int data_idx, index_idx;
 LIBMVL_VECTOR *vec_bits, *vec_first, *vec_first_mark, *vec_prev_mark, *vec_mark, *vec_prev, *vec, *vec_stats, *vec_max_count;
 SEXP ans, sa, sa2, R_fcall, tmp;
 double *values, *pd, *pd2;
 LIBMVL_NAMED_LIST *L;
-int *bits, Nbits;
-long long *first, *first_mark, *prev_mark, *mark, *prev, max_count, ball_size;
+int *bits;
+long long *first, *first_mark, *prev_mark, *mark, *prev, max_count;
 LIBMVL_VEC_STATS *vstats;
 char *ball;
 
@@ -4478,7 +4478,7 @@ Nbits=mvl_vector_length(vec_bits);
 
 if(Nbits!=length(data_list) || length(data_list)==0) {
 	mvl_free_named_list(L);
-	error("Query columns do not match spatial index: %d vs %d", length(data_list), Nbits);
+	error("Query columns do not match spatial index: %d vs %llu", length(data_list), Nbits);
 	return(R_NilValue);
 	}
 	
